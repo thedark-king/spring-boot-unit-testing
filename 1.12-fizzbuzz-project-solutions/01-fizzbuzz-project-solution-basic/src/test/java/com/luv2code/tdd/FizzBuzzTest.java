bf32dbafff8b79d447cb59ed33f52df4c22fbe5a
Ravi <thedevtool@gmail.com>
2023-12-20 12:29:08 +0530

    Testing test cases with the CSV file

--- 1.12-fizzbuzz-project-solutions/01-fizzbuzz-project-solution-basic/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
+++ 1.12-fizzbuzz-project-solutions/01-fizzbuzz-project-solution-basic/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -1,67 +1,79 @@
 package com.luv2code.tdd;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FizzBuzzTest {
 
     // If number is divisible by 3, print Fizz
-    @DisplayName("Divisible by Three")
-    @Test
-    @Order(1)
-    void testForDivisibleByThree() {
+    // If number is divisible by 5, print Buzz
+    // If number is divisible by 3 and 5, print FizzBuzz
+    // If number is NOT divisible by 3 and 5, print Number
 
+    @Test
+    @DisplayName("Divisible by Three")
+    void testForDivisibleByThree() {
         String expected = "Fizz";
 
-        assertEquals(expected, FizzBuzz.compute(3), "Should return Fizz");
+        assertEquals(expected, FizzBuzz.compute(3));
     }
 
-    // If number is divisible by 5, print Buzz
-    @DisplayName("Divisible by Five")
     @Test
-    @Order(2)
+    @DisplayName("Divisible by Five")
     void testForDivisibleByFive() {
-
         String expected = "Buzz";
 
-        assertEquals(expected, FizzBuzz.compute(5), "Should return Buzz");
+        assertEquals(expected, FizzBuzz.compute(50));
     }
 
-    // If number is divisible by 3 and 5, print FizzBuzz
-    @DisplayName("Divisible by Three and Five")
     @Test
-    @Order(3)
+    @DisplayName("Divisible by Three and Five")
     void testForDivisibleByThreeAndFive() {
-
         String expected = "FizzBuzz";
 
-        assertEquals(expected, FizzBuzz.compute(15), "Should return FizzBuzz");
+        assertEquals(expected, FizzBuzz.compute(15));
     }
 
-    // If number is NOT divisible by 3 or 5, then print the number
-    @DisplayName("Not Divisible by Three or Five")
     @Test
-    @Order(4)
+    @DisplayName("Not Divisible by Three or Five")
     void testForNotDivisibleByThreeOrFive() {
-
         String expected = "1";
 
-        assertEquals(expected, FizzBuzz.compute(1), "Should return 1");
+        assertEquals(expected, FizzBuzz.compute(1));
     }
 
-    @DisplayName("Testing with Small data file")
-    @ParameterizedTest(name="value={0}, expected={1}")
-    @CsvFileSource(resources="/small-test-data.csv")
+    @DisplayName("Testing with small data file")
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(resources = "/small-test-data.csv")
     @Order(5)
     void testSmallDataFile(int value, String expected) {
 
         assertEquals(expected, FizzBuzz.compute(value));
+    }
 
+    @DisplayName("Testing with small data file")
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(resources = "/medium-test-data.csv")
+    @Order(5)
+    void testMediumDataFile(int value, String expected) {
+
+        assertEquals(expected, FizzBuzz.compute(value));
+    }
+    @DisplayName("Testing with small data file")
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(resources = "/large-test-data.csv")
+    @Order(5)
+    void testLargeDataFile(int value, String expected) {
+
+        assertEquals(expected, FizzBuzz.compute(value));
     }
 
 }
