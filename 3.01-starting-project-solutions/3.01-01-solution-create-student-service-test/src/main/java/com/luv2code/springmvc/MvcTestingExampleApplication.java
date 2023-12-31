8a2f8249dd1473c4a01d5048ccc5593bfe0c19f3
Ravi <thedevtool@gmail.com>
2023-12-31 13:25:29 +0530

    refactoring the code

--- 3.01-starting-project-solutions/3.01-01-solution-create-student-service-test/src/main/java/com/luv2code/springmvc/MvcTestingExampleApplication.java
+++ 3.01-starting-project-solutions/3.01-01-solution-create-student-service-test/src/main/java/com/luv2code/springmvc/MvcTestingExampleApplication.java
@@ -10,41 +10,41 @@ import org.springframework.context.annotation.Scope;
 @SpringBootApplication
 public class MvcTestingExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MvcTestingExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MvcTestingExampleApplication.class, args);
+    }
 
-	@Bean
-	@Scope(value = "prototype")
-	CollegeStudent getCollegeStudent() {
-		return new CollegeStudent();
-	}
+    @Bean
+    @Scope(value = "prototype")
+    CollegeStudent getCollegeStudent() {
+        return new CollegeStudent();
+    }
 
-	@Bean
-	@Scope(value = "prototype")
-	Grade getMathGrade(double grade) {
-		return new MathGrade(grade);
-	}
+    @Bean
+    @Scope(value = "prototype")
+    Grade getMathGrade(double grade) {
+        return new MathGrade(grade);
+    }
 
-	@Bean
-	@Scope(value = "prototype")
-	@Qualifier("mathGrades")
-	MathGrade getGrade() {
-		return new MathGrade();
-	}
+    @Bean
+    @Scope(value = "prototype")
+    @Qualifier("mathGrades")
+    MathGrade getGrade() {
+        return new MathGrade();
+    }
 
-	@Bean
-	@Scope(value = "prototype")
-	@Qualifier("scienceGrades")
-	ScienceGrade getScienceGrade() {
-		return new ScienceGrade();
-	}
+    @Bean
+    @Scope(value = "prototype")
+    @Qualifier("scienceGrades")
+    ScienceGrade getScienceGrade() {
+        return new ScienceGrade();
+    }
 
-	@Bean
-	@Scope(value = "prototype")
-	@Qualifier("historyGrades")
-	HistoryGrade getHistoryGrade() {
-		return new HistoryGrade();
-	}
+    @Bean
+    @Scope(value = "prototype")
+    @Qualifier("historyGrades")
+    HistoryGrade getHistoryGrade() {
+        return new HistoryGrade();
+    }
 
 }
